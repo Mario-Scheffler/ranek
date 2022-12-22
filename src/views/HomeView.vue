675ccc3fc72e4dcf2328594f56ee57ff5ec9ac27
mario <xlikkipx@gmail.com>
2022-12-21 22:33:12 -0300

    criados componentes de produtos lista e busca

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,29 @@
 <template>
-  <div>
-
-  </div>
+  <section>
+    <h1>👍️️ Compre ou venda 👎️</h1>
+    <ProdutosBuscar />
+    <ProdutosLista />
+  </section>
 </template>
 
 <script>
-
+import ProdutosBuscar from '@/components/ProdutosBuscar.vue'
+import ProdutosLista from '@/components/ProdutosBuscar.vue'
 
 export default {
   name: 'HomeView',
+  components: {
+    ProdutosBuscar,
+    ProdutosLista
+  }
 }
 </script>
+
+<style scoped>
+h1 {
+  font-size: 2rem;
+  color: #87f;
+  margin-top: 40px;
+  text-align: center;
+}
+</style>
