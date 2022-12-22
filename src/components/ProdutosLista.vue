8c04298409968577b67d41adb7863354b2456893
mario <xlikkipx@gmail.com>
2022-12-22 18:49:54 -0300

    lista de produtos foi estilizada

--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -1,13 +1,18 @@
 <template>
   <section class="produtos-container">
-    <div v-for="produto in produtos" :key="produto.id">
-      <img v-if="produto.fotos" :src="produto.fotos[0].src" :alt="produto.fotos[0].titulo">
-      <h2 class="titulo">{{ produto.nome }}</h2>
-      <p class="preco"> {{ produto.preco }}</p>
-      <p> {{ produto.descricao }}</p>
-
+    <div v-if="produtos && produtos.length" class="produtos">
+      <div v-for="produto in produtos" :key="produto.id" class="produto">
+        <router-link to="/">
+          <img v-if="produto.fotos" :src="produto.fotos[0].src" :alt="produto.fotos[0].titulo">
+          <p class="preco"> {{ produto.preco }}</p>
+          <h2 class="titulo">{{ produto.nome }}</h2>
+          <p> {{ produto.descricao }}</p>
+        </router-link>
+      </div>
     </div>
-    {{ url }}
+    <div v-else-if="produtos && produtos.length === 0" class="sem-resultados">
+      <p>Busca sem resultados. Tente buscar outro termo.</p>
+    </div>
   </section>
 </template>
 
@@ -18,13 +23,14 @@ import { serialize } from '@/helpers.js'
 export default {
   data() {
     return {
-      produtos: null
-    }
+      produtos: null,
+      produtosPorPagina: 9,
+    };
   },
   computed: {
     url() {
       const query = serialize(this.$route.query);
-      return `/produto?_limit=10"${query}`;
+      return `/produto?_limit=${this.produtosPorPagina}"${query}`;
     }
   },
   methods: {
@@ -45,3 +51,50 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.produtos-container {
+  max-width: 1000px;
+  margin: 0 auto;
+}
+
+.produtos {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 30px;
+  margin: 30px;
+}
+
+.produto {
+  box-shadow: 0 4px 8px rgba(30, 60, 90, 0.1);
+  padding: 10px;
+  background: #fff;
+  border-radius: 4px;
+  transition: all 0.2s;
+}
+
+.produto:hover {
+  box-shadow: 0 6px 12px rgba(30, 60, 90, 0.2);
+  transform: scale(1.1);
+  position: relative;
+  z-index: 1;
+}
+
+.produto img {
+  border-radius: 4px;
+  margin-bottom: 20px;
+}
+
+.titulo {
+  margin-bottom: 10px;
+}
+
+.preco {
+  color: #e80;
+  font-weight: bold;
+}
+
+.sem-resultados {
+  text-align: center;
+}
+</style>
